ef85bc27f58a69d5117ef5dbefc455103a53e2d0
suprtrtl <owen.rodway@gmail.com>
2026-03-05 16:58:09 -0700

    added sorting method interface as well as bubble sort algorithm

---
+++ sorting_method.go
@@ -0,0 +1,34 @@
+package main
+
+type SortingMethod interface {
+	// Takes in array and returns a list of all total arrays ever modified from sorting,
+	// terrible space efficiency and should be replaced however better solution unclear
+	GetSortingSteps([]int) [][]int
+}
+
+type BubbleSort struct{}
+
+func (bs BubbleSort) GetSortingSteps(data []int) (steps [][]int) {
+	n := len(data)
+	// Also inefficient should get a size and capacity beforehand
+	steps = [][]int{data}
+
+	for i := range n {
+		swapped := false
+		for j := range n - i - 1 {
+			if data[j] > data[j+1] {
+				data[j], data[j+1] = data[j+1], data[j]
+				swapped = true
+
+				// Add array to steps list
+				steps = append(steps, data)
+			}
+		}
+
+		if (swapped == false) {
+			break
+		}
+	}
+
+	return steps
+}
