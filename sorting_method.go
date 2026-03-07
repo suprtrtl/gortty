24dba3e85844fd9047c1bdf5adf90bfcb1ec9779
Andrei <a@ndrei.dev>
2026-03-06 17:07:42 -0700

    Added more sorting methods

--- sorting_method.go
+++ sorting_method.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"math/rand"
 	"time"
 )
 
 // Holds Whether or not array is sorted
 type RenderStepMsg bool
 
-
 type SortingMethod interface {
 	Sort(model)
 }
 
+type BogoSort struct{}
 type BubbleSort struct{}
+type QuickSort struct{}
+type SelectionSort struct{}
+
+func (bg BogoSort) Sort(m model) {
+	for {
+		sorted := true
+		for index := 0; index < len(m.data)-1; index++ {
+			if m.data[index] > m.data[index+1] {
+				sorted = false
+			}
+		}
+
+		if sorted {
+			m.program.Send(RenderStepMsg(true))
+			return
+		} else {
+			rand.Shuffle(len(m.data), func(i, j int) {
+				m.data[i], m.data[j] = m.data[j], m.data[i]
+			})
+			m.program.Send(RenderStepMsg(false))
+		}
+
+		time.Sleep(time.Millisecond * time.Duration(m.delay))
+	}
+}
 
 func (bs BubbleSort) Sort(m model) {
 	for itr := range m.data {
@@ -21,6 +47,7 @@ func (bs BubbleSort) Sort(m model) {
 			if m.data[index] > m.data[index+1] {
 				sorted = false
 				m.data[index], m.data[index+1] = m.data[index+1], m.data[index]
+
 				m.program.Send(RenderStepMsg(false))
 				time.Sleep(time.Millisecond * time.Duration(m.delay))
 			}
@@ -31,4 +58,21 @@ func (bs BubbleSort) Sort(m model) {
 			return
 		}
 	}
+}
+
+func (ss SelectionSort) Sort(m model) {
+	for itr := 0; itr < len(m.data)-1; itr++ {
+		minIndex := itr
+		for index := itr + 1; index < len(m.data); index++ {
+			if m.data[minIndex] > m.data[index] {
+				minIndex = index
+			}
+		}
+
+		m.data[itr], m.data[minIndex] = m.data[minIndex], m.data[itr]
+		m.program.Send(RenderStepMsg(false))
+		time.Sleep(time.Millisecond * time.Duration(m.delay))
+	}
+
+	m.program.Send(RenderStepMsg(true))
 }
